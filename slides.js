b9eb0acb157de951990b69e60ae128863e59afc4
Mark Asfour <mj4sparrow@gmail.com>
2016-03-21 16:13:23 -0700

    started project slider

---
+++ slides.js
@@ -0,0 +1,21 @@
+$(".slider").slick({
+
+    autoplay: true,
+    dots: true,
+    customPaging : function(slider, i) {
+        var thumb = $(slider.$slides[i]).data('thumb');
+        return '<a><img src="'+thumb+'"></a>';
+    },
+
+    responsive: [{ 
+        breakpoint: 500,
+        settings: {
+            dots: false,
+            arrows: false,
+            infinite: false,
+            slidesToShow: 2,
+            slidesToScroll: 2
+        } 
+    }]
+});
+
