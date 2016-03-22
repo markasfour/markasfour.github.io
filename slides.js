7cb033cc8ab0af97e476c6c745db8e4fa392d471
Mark Asfour <mj4sparrow@gmail.com>
2016-03-21 21:44:52 -0700

    got slider working

--- slides.js
+++ slides.js
@@ -2,19 +2,16 @@ $(".slider").slick({
 
     autoplay: true,
     dots: true,
-    customPaging : function(slider, i) {
-        var thumb = $(slider.$slides[i]).data('thumb');
-        return '<a><img src="'+thumb+'"></a>';
-    },
-
+    //customPaging : function(slider, i) {
+    //    var thumb = $(slider.$slides[i]).data('thumb');
+    //    return '<a><img src="'+thumb+'"></a>';
+    //},
+	
     responsive: [{ 
         breakpoint: 500,
         settings: {
-            dots: false,
             arrows: false,
             infinite: false,
-            slidesToShow: 2,
-            slidesToScroll: 2
         } 
     }]
 });
